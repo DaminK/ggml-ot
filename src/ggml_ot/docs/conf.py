b778c19c392bb35a05ab5e70df56ca0f491d6626
Simon Jonas <simon.jonas@rwth-aachen.de>
2025-04-11 16:20:43 +0200

    prep work for readthedocs and metadata

---
+++ src/ggml_ot/docs/conf.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+# Default settings
+
+project = "ggml-ot"
+
+extensions = [
+    "sphinx_autorun",
+]
+
+
+latex_engine = "xelatex"  # allow us to build Unicode chars
+
+html_theme = "sphinx_rtd_theme"
