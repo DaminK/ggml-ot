3dc7d1c25cfebc8b0b259668d27c666d834c18aa
Simon Jonas <simon.jonas@rwth-aachen.de>
2025-04-13 13:54:28 +0200

    precommit stuff & sphinx setup

---
+++ docs/source/conf.py
@@ -0,0 +1,11 @@
+project = "ggml-ot"
+author = "Damin Kuehn <damin.kuehn@rwth-aachen.de>"
+release = "0.0.1"
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+html_theme = "sphinx_rtd_theme"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.duration",
+    "sphinx_autorun",
+]
