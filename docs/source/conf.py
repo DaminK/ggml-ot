4e1d01d51c0155661f29fae89d9ff7e6384e2ad2
Damin <damin.kuehn@rwth-aachen.de>
2025-04-30 15:51:07 +0200

    v0.0.1

---
+++ docs/source/conf.py
@@ -0,0 +1,18 @@
+import os
+
+project = "ggml-ot"
+author = "Damin Kuehn <damin.kuehn@rwth-aachen.de>"
+release = "0.0.1"
+
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+html_theme = "sphinx_rtd_theme"
+
+master_doc = "index"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.duration",
+    "sphinx_autorun",
+    "myst_parser",
+]
